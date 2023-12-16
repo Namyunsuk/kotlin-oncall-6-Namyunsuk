1564f61581e0e5957f01e8573b59ddae039335cb
Namyunsuk <yunsuk0328@naver.com>
2023-12-16 15:00:00 +0900

    feat(Calendar): 법정 공휴일 정보 저장 및 달 별로 날짜및 요일 계산 및 저장 기능 구현

---
+++ src/main/kotlin/oncall/utils/Calendar.kt
@@ -0,0 +1,46 @@
+package oncall.utils
+
+enum class Calendar(val month: Int, val date: Int) {
+    FIRST(1, 1),
+    SECOND(3, 1),
+    THIRD(5, 5),
+    FOURTH(6, 6),
+    FIFTH(8, 15),
+    SIX(10, 3),
+    SEVENTH(10, 9),
+    EIGHT(12, 15);
+
+    companion object {
+        val day = listOf("월", "화", "수", "목", "금", "토", "일")
+
+        fun matchHoliday(month: Int, date: Int): Boolean {
+            val matchingEnum = entries.find { it.month == month && it.date == date }
+            return matchingEnum != null
+        }
+
+        fun findLastDayOfMonth(month: Int): Int {
+            val daysInMonth = when (month) {
+                1, 3, 5, 7, 8, 10, 12 -> 31
+                4, 6, 9, 11 -> 30
+                2 -> 28
+                else -> -1
+            }
+            return daysInMonth
+        }
+
+        fun findDayOfWeek(month: Int, daysInMonth: Int, dayOfWeek: String): Map<Int, String> {
+            val dayAndDate = mutableMapOf<Int, String>()
+            val dayNames = arrayOf("일", "월", "화", "수", "목", "금", "토")
+            var currentDayOfWeek = dayNames.indexOf(dayOfWeek)
+            for (day in 1..daysInMonth) {
+                if (matchHoliday(month, day)) {
+                    dayAndDate[day] = dayNames[currentDayOfWeek] + "(휴일)"
+                    continue
+                }
+                dayAndDate[day] = dayNames[currentDayOfWeek]
+                currentDayOfWeek = (currentDayOfWeek + 1) % 7
+            }
+            return dayAndDate
+        }
+    }
+}
