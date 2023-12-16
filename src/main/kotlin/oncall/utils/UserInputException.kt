637d35cfdb5f573d8e65d6c789629f247831c5a0
Namyunsuk <yunsuk0328@naver.com>
2023-12-16 16:49:24 +0900

    feat: 예외처리 사항 구현

--- src/main/kotlin/oncall/utils/UserInputException.kt
+++ src/main/kotlin/oncall/utils/UserInputException.kt
@@ -1,4 +1,55 @@
 package oncall.utils
 
+import camp.nextstep.edu.missionutils.Console
+
 class UserInputException {
+    companion object {
+
+        fun checkMonthAndDay(userInput: String): Pair<Int, String> {
+            val (month, day) = userInput.split(",")
+            require(checkNumber(month)) { "[ERROR] 월은 숫자로 입력해야 합니다." }
+            require(checkValidMonth(month.toInt())) { "[ERROR] 월은 1~12사이 숫자여야 합니다." }
+            require(checkValidDay(day)) { "[ERROR] 유효하지 않은 요일입니다." }
+            return Pair(month.toInt(), day)
+        }
+
+        fun checkWorTurn(userInput: String): List<String> {
+            val emergencyTurn = userInput.split(",")
+            require(checkNameLen(emergencyTurn)) { "[ERROR] 닉네임은 5자 이하여야 합니다." }
+            require(checkDuplicate(emergencyTurn)) { "[ERROR] 두번 편성될 수는 없습니다." }
+            require(checkWorkerNum(emergencyTurn)) { "[ERROR] 근무자는 5~35명이어야 합니다." }
+            return emergencyTurn
+        }
+
+        fun checkDuplicate(userNumbers: List<String>): Boolean {
+            val distinctUserNumbers = userNumbers.distinct()
+            return userNumbers.size == distinctUserNumbers.size
+        }
+
+        fun checkNameLen(workerName: List<String>): Boolean {
+            return workerName.all { it.length <= 5 }
+        }
+
+        fun checkNumber(userInput: String): Boolean {
+            for (numberIndex in userInput.indices) {
+                if (userInput[numberIndex].code < '0'.code || userInput[numberIndex].code > '9'.code) {
+                    return false
+                }
+            }
+            return true
+        }
+
+        fun checkWorkerNum(userInput: List<String>): Boolean {
+            return userInput.size in 5..35
+        }
+
+        fun checkValidMonth(userInput: Int): Boolean {
+            return userInput in 1..12
+        }
+
+        fun checkValidDay(userInput: String): Boolean {
+            val dayNames = arrayOf("일", "월", "화", "수", "목", "금", "토")
+            return userInput in dayNames
+        }
+    }
 }
--- src/main/kotlin/oncall/view/InputView.kt
+++ src/main/kotlin/oncall/view/InputView.kt
@@ -1,28 +1,44 @@
 package oncall.view
 
 import camp.nextstep.edu.missionutils.Console
+import oncall.utils.UserInputException
 
 class InputView {
     companion object {
         fun enterMonthAndDay(): Pair<Int, String> {
             print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ")
-            val userInput = Console.readLine()
-            val (month, day) = userInput.split(",")
-            return Pair(month.toInt(), day)
+            while (true) {
+                val userInput = Console.readLine()
+                try {
+                    return UserInputException.checkMonthAndDay(userInput)
+                } catch (e: IllegalArgumentException) {
+                    print(e.message)
+                }
+            }
         }
 
-        fun enterWeekdayTurn():List<String>{
+        fun enterWeekdayTurn(): List<String> {
             print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
-            val userInput = Console.readLine()
-            val weekdayTurn = userInput.split(",")
-            return weekdayTurn
+            while (true) {
+                val userInput = Console.readLine()
+                try {
+                    return UserInputException.checkWorTurn(userInput)
+                } catch (e: IllegalArgumentException) {
+                    print(e.message)
+                }
+            }
         }
 
-        fun enterHolidayTurn():List<String>{
+        fun enterHolidayTurn(): List<String> {
             print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
-            val userInput = Console.readLine()
-            val holidayTurn = userInput.split(",")
-            return holidayTurn
+            while (true) {
+                val userInput = Console.readLine()
+                try {
+                    return UserInputException.checkWorTurn(userInput)
+                } catch (e: IllegalArgumentException) {
+                    print(e.message)
+                }
+            }
         }
     }
 }
