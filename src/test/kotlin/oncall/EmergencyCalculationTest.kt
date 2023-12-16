45c558eea96adc7fb2eebf9683a48ba8d01ead09
Namyunsuk <yunsuk0328@naver.com>
2023-12-16 18:14:08 +0900

    test(EmergencyCalculationTest): 두번 연속 근무 시 순서를 바꾸는지 테스트 구현

--- src/test/kotlin/oncall/EmergencyCalculationTest.kt
+++ src/test/kotlin/oncall/EmergencyCalculationTest.kt
@@ -1,4 +1,13 @@
 package oncall
 
+import oncall.model.EmergencyCalculation
+
+import org.assertj.core.api.Assertions
+
 class EmergencyCalculationTest {
+    fun `두번 연속 근무하는 경우 순서를 바꾸는지 테스트`() {
+        val emergencyCalculation = EmergencyCalculation()
+        val result = emergencyCalculation.duplicateSwitch("조랑말", mutableListOf("조랑말", "얼룩말"))
+        Assertions.assertThat(result).isEqualTo(mutableListOf("얼룩말", "조랑말"))
+    }
 }
